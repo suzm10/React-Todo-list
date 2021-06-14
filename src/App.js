41ed64ce6cf45ab29558ff0f9c6528c30bb33355
suzm10 <suzanmanasreh@gmail.com>
2021-06-14 12:41:52 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
-import './App.css';
-import Input from '@material-ui/core/Input';
-import { makeStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
+import React from "react";
+
 
 function App() {
   return (
-    <div className="App">
-      <Input defaultValue="Input" inputProps={{ 'aria-label': 'description' }} />
+    <div>
+      <h1>App</h1>
     </div>
-    /*
-    <div className="List">
-    <List component="nav" aria-label="secondary mailbox folders">
-      <ListItem button>
-        <ListItemText primary="Item 1" />
-      </ListItem>
-      <ListItemLink href="#simple-list">
-        <ListItemText primary="Item 2" />
-      </ListItemLink>
-    </List>
-  </div>
-  */
   );
 }
 
