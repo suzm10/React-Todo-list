e19dac527467b1efc96daff2a776193c5656c020
suzm10 <suzanmanasreh@gmail.com>
2021-06-14 19:02:44 -0400

    todo component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import TodoForm from "./components/TodoForm"
+import TodoList from "./components/TodoList"
 
 function App() {
   return (
     <div>
-      <TodoForm/>
+      <TodoForm />
+      <TodoList />
     </div>
   );
 }
---
+++ src/components/Todo.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Card, CardContent, Container, Typography, IconButton } from '@material-ui/core'
+
+const Todo = () => {
+    return (
+        <div>
+            <Container>
+                <Card variant="outlined">
+
+                </Card>
+            </Container>
+        </div>
+    )
+}
+
+export default Todo
