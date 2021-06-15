b09f1e6f1f7a091b62f7228ae24d0abfbeb530ca
Gabriel Dutra <57206480+Dutra-Apex@users.noreply.github.com>
2021-06-15 18:17:37 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
+import React, {useState} from 'react';
+import {v4} from 'uuid'
 import Header from "./component/header"
+import ToDoForm from "./component/ToDoForm"
+import ToDoList from './component/ToDoList';
+//import ToDo from './component/ToDo';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([
+
+  ])
+
+  const addTodo= (text) => {
+      const newTodo = {
+        id: v4(),
+        title: text,
+        isCompleted: false,
+      }
+      setTodos([...todos, newTodo])
+  }
+
+
+  const crossTask = (id) => {
+    setTodos(
+      todos.map(todo => {
+      if(todo.id === id)
+      todo.isCompleted =! todo.isCompleted;
+      return todo
+    }))
+  }
+
+  const deleteTask = (id) => {
+    setTodos(todos.filter(todo => todo.id != id))
+  }
+
+
   return (
     <div className="App">
     <Header/>
+    <ToDoForm addTodo ={addTodo} />
+    <ToDoList todos={todos} 
+    crossTask={crossTask}
+    deleteTask = {deleteTask}/>
     </div>
   );
 }
