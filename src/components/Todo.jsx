2ea61affbc5c549f318e087a6b00b5b526d9ec5a
suzm10 <suzanmanasreh@gmail.com>
2021-06-19 19:48:22 -0400

    edit label

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { useState } from 'react'
 import { Card, CardContent, Container, Typography, IconButton, Chip, Grid } from '@material-ui/core'
 import { Check, Delete, Edit } from '@material-ui/icons'
+import EditableLabel from 'react-inline-editing'
+
 
 const Todo = ({ title, checkTodo, id, isCompleted, deleteTodo, editTodo, date }) => {
     const currentDate = date
@@ -18,12 +20,11 @@ const Todo = ({ title, checkTodo, id, isCompleted, deleteTodo, editTodo, date })
             <Container>
                 <Card variant="outlined" style={{ marginTop: 35, background: "#ECECEC" }}>
                     <CardContent>
-                    {/* Check Icon */}
                         <Typography variant="h5" component="h2" style={todoStyle} spacing={5}>
                             <IconButton onClick={markComplete}>
                                 <Check style={{ color: "#28CC2D" }}/>
                             </IconButton>
-                            {title}
+                            <EditableLabel text = {title}></EditableLabel>
                             <Chip label={date} style={{ color: "#3581D8" }}/>
                             <IconButton style={{ float: "right" }}>
                                 <Edit style={{ color: "#FFE135" }} onClick={editTodo}/>
